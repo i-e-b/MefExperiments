261a28fcf3340f8dcbf8677762740ad6cc69219d
Iain Ballard <iain.ballard@gmail.com>
2014-01-20 14:42:47 +0000

    A thread-safe and working plugin watcher, but its *really* ugly

---
+++ MefExperiment/PluginWatcher/PluginWatcher.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using PluginWatcher.Contracts;
+
+namespace PluginWatcher
+{
+    /// <summary>
+    /// Watches for a fixed kind of plugin
+    /// </summary>
+    /// <remarks>
+    /// The mix of attributes, exceptions, thread safety problems and odd workflow makes this 
+    /// behaviour very tricky to implement, and full of gotchas.
+    /// Every weird implementation detail below is the result of trial-and-error.
+    /// Change it at your peril.
+    /// <para>How it works:</para>
+    /// A file system watcher waits for *.dll changes in the target directory. On each change, the DirectoryCatalog will be refreshed.
+    /// That triggers `ExportsChanged`, which may flag `_reload`. If reload is set to true, the update thread will recompose the list of plugins.
+    /// </remarks>
+    public class PluginWatcher : IDisposable
+    {
+        [ImportMany(typeof(IWatchablePlugin), AllowRecomposition = true)]
+        protected IEnumerable<IWatchablePlugin> Plugins { get; set; }
+
+
+        private static readonly object CompositionLock = new object();
+        private volatile bool _reload;
+        private volatile bool _running;
+
+        private readonly string _pluginDirectory;
+        private FileSystemWatcher _fsw;
+        private Thread _updateThread;
+        private DirectoryCatalog _pluginCatalog;
+
+        public event EventHandler<PluginsChangedEventArgs> PluginsChanged;
+
+        protected virtual void OnPluginsChanged()
+        {
+            var handler = PluginsChanged;
+            if (handler != null) handler(this, new PluginsChangedEventArgs { AvailablePlugins = CurrentlyAvailable });
+        }
+
+        public PluginWatcher(string pluginDirectory)
+        {
+            _pluginDirectory = pluginDirectory;
+            if (!Directory.Exists(_pluginDirectory)) throw new Exception("Can't watch \"" + _pluginDirectory + "\", might not exist or not enough permissions");
+
+            CurrentlyAvailable = new IWatchablePlugin[0];
+            _fsw = new FileSystemWatcher(_pluginDirectory, "*.dll");
+            SetupFileWatcher();
+
+            _reload = true;
+            _running = true;
+
+            _updateThread = new Thread(UpdateThreadLoop) { IsBackground = true };
+            _updateThread.Start();
+
+            while (_reload) { Thread.Sleep(100); }
+        }
+
+        private void UpdateThreadLoop()
+        {
+            _pluginCatalog = new DirectoryCatalog("./Plugins");
+            try
+            {
+                using (var localCatalog = new AssemblyCatalog(Assembly.GetExecutingAssembly()))
+                using (var catalog = new AggregateCatalog())
+                {
+                    catalog.Catalogs.Add(localCatalog);
+                    catalog.Catalogs.Add(_pluginCatalog);
+
+                    using (var container = new CompositionContainer(catalog, false))
+                    {
+                        container.ExportsChanged += ExportsChanged;
+
+                        while (_running)
+                        {
+                            lock (CompositionLock)
+                            {
+                                if (_reload)
+                                {
+                                    container.ComposeParts(this);
+                                    CurrentlyAvailable = (Plugins ?? new IWatchablePlugin[0]).ToArray();
+
+                                    _reload = false;
+                                    OnPluginsChanged();
+                                }
+                            }
+                            while (_running && !_reload) Thread.Sleep(1000);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                lock (CompositionLock)
+                {
+                    var cat = Interlocked.Exchange(ref _pluginCatalog, null);
+                    if (cat != null) cat.Dispose();
+                }
+            }
+        }
+
+        private void SetupFileWatcher()
+        {
+            _fsw.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.FileName |
+                                NotifyFilters.LastAccess | NotifyFilters.LastWrite    | NotifyFilters.Size     | NotifyFilters.Security;
+
+            _fsw.Changed += FileAddedOrRemoved;
+            _fsw.Created += FileAddedOrRemoved;
+            _fsw.Deleted += FileAddedOrRemoved;
+            _fsw.Renamed += FileRenamed;
+
+            _fsw.EnableRaisingEvents = true;
+        }
+
+
+        private void ExportsChanged(object sender, ExportsChangeEventArgs e)
+        {
+            lock (CompositionLock)
+            {
+                // if anything changed, trigger a rebuild (can't do it on the event thread)
+                if (e.AddedExports.Any() || e.RemovedExports.Any()) _reload = true;
+            }
+        }
+
+        private void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            RefreshPlugins();
+        }
+
+        void FileAddedOrRemoved(object sender, FileSystemEventArgs e)
+        {
+            RefreshPlugins();
+        }
+
+        private void RefreshPlugins()
+        {
+            try
+            {
+                var cat = _pluginCatalog;
+                if (cat == null) { return; }
+                lock (CompositionLock)
+                {
+                    cat.Refresh();
+                }
+            }
+            catch (ChangeRejectedException rejex)
+            {
+                Console.WriteLine("Could not update plugins: " + rejex.Message);
+            }
+        }
+
+        public IEnumerable<IWatchablePlugin> CurrentlyAvailable { get; protected set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            _running = false;
+            var fsw = Interlocked.Exchange(ref _fsw, null);
+            if (fsw != null) fsw.Dispose();
+
+            var thread = Interlocked.Exchange(ref _updateThread, null);
+            if (thread != null)
+            {
+                thread.Join(2000);
+            }
+        }
+    }
+
+    public class PluginsChangedEventArgs: EventArgs
+    {
+        public IEnumerable<IWatchablePlugin> AvailablePlugins { get; set; }
+    }
+}
--- MefExperiment/PluginWatcher/Program.cs
+++ MefExperiment/PluginWatcher/Program.cs
@@ -1,94 +1,36 @@
 ﻿using System;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 
 namespace PluginWatcher
 {
     public class Program
     {
-        private static CompositionContainer _container;
-        private static readonly object CompositionLock = new object();
-        private static bool _reload;
-        private static PluginContainer _loadedPlugins;
-
-
         static void Main()
         {
             Console.WriteLine("As plugins are added and removed, you should see output below. Press [enter] to exit");
 
-            var pluginCatalog = new DirectoryCatalog("./Plugins");
-            var localCatalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var watcher = new PluginWatcher("./Plugins");
+            watcher.PluginsChanged += watcher_PluginsChanged;
 
-            var catalog = new AggregateCatalog();
-            catalog.Catalogs.Add(localCatalog);
-            catalog.Catalogs.Add(pluginCatalog);
 
-            _container = new CompositionContainer(catalog, false);
-            _container.ExportsChanged += ExportsChanged;
+            Console.WriteLine("\r\nPlugins loaded:");
+            Console.WriteLine(string.Join(", ", watcher.CurrentlyAvailable.Select(p => p.Name())));
 
-            _reload = true;
-
-            try
+            do
             {
-                do
+                while (!Console.KeyAvailable)
                 {
-                    while (!Console.KeyAvailable)
-                    {
-                        lock (CompositionLock)
-                        {
-                            Thread.Sleep(200);
-                            try
-                            {
-                                pluginCatalog.Refresh();
-                            }
-                            catch (ChangeRejectedException rejex)
-                            {
-                                Console.WriteLine("Could not update plugins: " + rejex.Message);
-                            }
-                        }
-
-                        lock (CompositionLock)
-                        {
-                            if (_reload)
-                            {
-                                _reload = false;
-                                ListKnownPlugins();
-                            }
-                        }
-                    }
+                    Thread.Sleep(200);
                 }
-                while (Console.ReadKey().Key != ConsoleKey.Enter);
-
             }
-            finally
-            {
-                pluginCatalog.Dispose();
-                localCatalog.Dispose();
-                catalog.Dispose();
-                _container.Dispose();
-            }
+            while (Console.ReadKey().Key != ConsoleKey.Enter);
         }
 
-        static void ExportsChanged(object sender, ExportsChangeEventArgs e)
+        static void watcher_PluginsChanged(object sender, PluginsChangedEventArgs e)
         {
-            lock (CompositionLock)
-            {
-                if (e.AddedExports.Any() || e.RemovedExports.Any()) _reload = true;
-            }
-        }
-
-        private static void ListKnownPlugins()
-        {
-            _loadedPlugins = new PluginContainer();
-            lock (CompositionLock)
-            {
-                _container.ComposeParts(_loadedPlugins);
-            }
             Console.WriteLine("\r\nPlugins reloaded:");
-            Console.WriteLine(string.Join(", ", _loadedPlugins.Plugins.Select(p => p.Name())));
+            Console.WriteLine(string.Join(", ", e.AvailablePlugins.Select(p => p.Name())));
         }
     }
 }
